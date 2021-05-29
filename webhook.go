1c6cc76f386dc6876bcfcfb82e04298e4edbbe15
Takatoshi MATSUO <matsuo.tak@gmail.com>
2021-05-29 22:54:02 +0900

    improve log output

--- webhook.go
+++ webhook.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -15,45 +16,78 @@ import (
 )
 
 var goenv struct {
-	Cmd  string `required:"true"`
-	Port int    `default:"22999"`
-	Bind string `default:"0.0.0.0"`
-	Path string `default:"/"`
+	Cmd        string `required:"true"`
+	Port       int    `default:"22999"`
+	Bind       string `default:"0.0.0.0"`
+	Path       string `default:"/"`
+	Debug      bool   `default:"false"`
+	Log_prefix string `default:"[webhook]"`
+	No_alog    bool   `default:"false"`
 }
+
+var log_info *log.Logger
+var log_err *log.Logger
+var log_debug *log.Logger
+var log_access *log.Logger
 
 func handleEnv() {
 	if err := envconfig.Process("webhook", &goenv); err != nil {
-		log.Fatalf("ERROR: Failed to process env: %s", err)
+		log_err.Fatalf("Failed to process env: %s", err)
 		os.Exit(1)
 	}
+
+	if goenv.Debug == true {
+		log_info.SetFlags(log.LstdFlags | log.Llongfile | log.Lmsgprefix)
+		log_err.SetFlags(log.LstdFlags | log.Llongfile | log.Lmsgprefix)
+		log_debug.SetOutput(os.Stderr)
+		log_access.SetFlags(log.LstdFlags | log.Llongfile | log.Lmsgprefix)
+	}
+
+	if goenv.No_alog == true {
+		log_access.SetOutput(ioutil.Discard)
+	}
+
+	log_info.SetPrefix(goenv.Log_prefix + " INFO ")
+	log_err.SetPrefix(goenv.Log_prefix + " ERROR ")
+	log_debug.SetPrefix(goenv.Log_prefix + " DEBUG ")
+	log_access.SetPrefix(goenv.Log_prefix + " ACCESS ")
 
 	_, err := os.Stat(goenv.Cmd)
 	if os.IsNotExist(err) {
-		log.Fatal("ERROR: %s command not found\n", goenv.Cmd)
+		log_err.Fatalf("%s command not found\n", goenv.Cmd)
 	}
 
 	if goenv.Port <= 0 || goenv.Port > 65535 {
-		fmt.Printf("ERROR: Invalid port number %d\n", goenv.Port)
-		os.Exit(1)
+		log_err.Fatalf("Invalid port number %d\n", goenv.Port)
 	}
 
 	if net.ParseIP(goenv.Bind) == nil {
-		fmt.Println("ERROR: Invalid bind IP " + goenv.Bind)
-		os.Exit(1)
+		log_err.Fatalf("Invalid bind IP %s\n", goenv.Bind)
 	}
 
 	if !regexp.MustCompile(`^/[0-9A-z/\-]*$`).MatchString(goenv.Path) {
-		fmt.Println("ERROR: Invalid Path " + goenv.Path)
-		os.Exit(1)
+		log_err.Fatalf("Invalid Path %s\n", goenv.Path)
 	}
 
 	script := os.Getenv("WEBHOOK_CMD")
-	fmt.Println("Command: " + script)
-	fmt.Printf("Webhook listening at: http://%s:%d%s\n", goenv.Bind, goenv.Port, goenv.Path)
+	log_info.Printf("Command is %s\n", script)
+	log_info.Printf("Listening on http://%s:%d%s\n", goenv.Bind, goenv.Port, goenv.Path)
+}
+
+func GetIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
 }
 
 func handleReq(w http.ResponseWriter, r *http.Request) {
+	ip := GetIP(r)
+	alog_format := ip
+
 	if r.URL.Path != goenv.Path {
+		log_access.Printf("%s Invalid Path: %s\n", alog_format, r.URL.Path)
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
@@ -64,15 +98,25 @@ func handleReq(w http.ResponseWriter, r *http.Request) {
 		bufbody.ReadFrom(r.Body)
 		body := bufbody.String()
 		if body == "" {
-			fmt.Print("POST body is empty")
+			log_access.Printf("%s Post: '%v'\n", alog_format, body)
+			fmt.Fprintf(w, "Not called.\n")
 			return
 		}
 
-		fmt.Printf("POST body : '%v'\n", body)
+		log_access.Printf("%s Post: '%v'\n", alog_format, body)
 		exec.Command(goenv.Cmd, body).Start()
+		fmt.Fprintf(w, "Called.\n")
 	default:
+		log_access.Printf("%s Invalid method: %s\n", alog_format, r.Method)
 		fmt.Fprintf(w, "Invalid method.\n")
 	}
+}
+
+func init() {
+	log_info = log.New(os.Stdout, "[webhook] INFO ", log.LstdFlags|log.Lmsgprefix)
+	log_err = log.New(os.Stderr, "[webhook] ERROR ", log.LstdFlags|log.Lmsgprefix)
+	log_debug = log.New(ioutil.Discard, "[webhook] DEBUG ", log.LstdFlags|log.Llongfile|log.Lmsgprefix)
+	log_access = log.New(os.Stdout, "[webhook] ACCESS ", log.LstdFlags|log.Llongfile|log.Lmsgprefix)
 }
 
 func main() {
@@ -80,6 +124,6 @@ func main() {
 	http.HandleFunc(goenv.Path, handleReq)
 
 	if err := http.ListenAndServe(goenv.Bind+":"+strconv.Itoa(goenv.Port), nil); err != nil {
-		log.Fatal(err)
+		log_err.Fatal(err)
 	}
 }
